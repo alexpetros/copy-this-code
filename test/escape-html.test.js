3f67793df672dc05023bbb83053c0b650968ef59
alexpetros <apetros15@gmail.com>
2023-02-26 17:37:13 -0500

    Add sleep and escape tests

---
+++ test/escape-html.test.js
@@ -0,0 +1,19 @@
+import test from 'node:test'
+import assert from 'node:assert/strict'
+import * as escapeHtml from '../lib/escape-html.js'
+
+test('escapeHtmlText', () => {
+  test('It properly escapes a script tag', () => {
+    const maliciousString = '<script>alert()</script>'
+    const escapedString = escapeHtml.escapeHtmlText(maliciousString)
+    assert.equal(escapedString, '&lt;script&gt;alert()&lt;&#x2F;script&gt;')
+  })
+})
+
+test('html template string', () => {
+  test('It properly escapes a script tag', () => {
+    const maliciousString = '<script>alert()</script>'
+    const templateString = escapeHtml.html`<li>${maliciousString}`
+    assert.equal(templateString, '<li>&lt;script&gt;alert()&lt;&#x2F;script&gt;')
+  })
+})
---
+++ test/sleep.test.js
@@ -0,0 +1,13 @@
+import test from 'node:test'
+import assert from 'node:assert/strict'
+import * as sleep from '../lib/sleep.js'
+
+test('it sleeps for 100 ms', async () => {
+  const startTime = (new Date()).getTime()
+  await sleep.sleep(100)
+  const endTime = (new Date()).getTime()
+
+  // Include 5ms of leeway
+  const success = Math.abs(endTime - startTime - 100) < 5
+  assert(success)
+})
