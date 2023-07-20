8f3fc75cc71c43afa23fd633e529cbb2fc192e5e
Alexander Petros <apetros15@gmail.com>
2023-07-20 10:28:09 -0400

    Add bash script starter

---
+++ bash/basic.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+set -euo pipefail
+
+# Allow tracing with `TRACE=1 ./basic.sh`
+if [[ "${TRACE-0}" == "1"  ]]; then set -o xtrace; fi
+
+error() {
+    echo "[$(date +'%Y-%m-%dT%H:%M:%S')]: $*" >&2
+}
+
+# Change script directory to script location
+cd "$(dirname "$0")"
+
+echo "This is the start of a basic bash script"
