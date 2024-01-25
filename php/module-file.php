d290f6df7017aaa7616f23ea285f08d64bffe9ad
Alexander Petros <apetros15@gmail.com>
2024-01-25 17:27:16 -0500

    Add gnat php snippet

---
+++ php/module-file.php
@@ -0,0 +1,5 @@
+/*
+ * Put this at the top of module files so that it's not directly callable
+ * From: Nathaniel Sabanski, with permission
+ */
+<? if(strstr(__FILE__, $_SERVER['PHP_SELF'])) die("404") ?>
