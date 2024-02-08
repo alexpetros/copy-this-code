19b519bdc06a93f9603ffa03c7c42f16fe4dbfb7
Alexander Petros <apetros15@gmail.com>
2024-02-08 11:23:39 -0500

    Make S3 download more configurable

--- bash/aws-s3-download.sh
+++ bash/aws-s3-download.sh
@@ -1,17 +1,21 @@
 #!/bin//bash
-# This script requires the aws CLI to be installed
-
+# Download an S3 bucket, with timing
+# Requires the aws CLI to be installed
 set -euo pipefail
+
+s3_bucket="s3://example-bucket/subdir"
+dest="./out"
+tmp_dest="$dest-tmp"
+
 start=$SECONDS
 
 # Get AWS_ACCESS_KEY_ID and AWS_SECRET_ACCESS_KEY from your .env
-# And put them in the shell environment
+# If you already have these in your environment, comment this line out
 export $(cat .env | xargs)
 
-aws s3 cp s3://example-bucket/logs ./new-logs --recursive
-rm -rf ./logs
-mv ./new-logs logs
+aws s3 cp "$s3_bucket" "$tmp_dest" --recursive
+rm -rf "$dest"
+mv "$tmp_dest" "$dest"
 
 duration=$(( SECONDS - start ))
-echo Finished downloading in $duration
-
+echo Finished downloading in $duration seconds
