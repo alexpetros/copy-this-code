5282b27191e8134eb1662ee581c21bcabfee7ac5
Alexander Petros <apetros15@gmail.com>
2024-02-28 23:29:13 -0500

    Switch to PHP setup script

--- cloud/digitalocean-php-setup.sh
+++ cloud/digitalocean-php-setup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #
-# Digital Ocean Webserver Setup
-# Tested on Ubuntu 23.10 x64 and Ubuntu 22.04 (LTS) x64
+# Digital Ocean PHP Server Setup
+# Tested on Ubuntu 22.04 (LTS) x64
 set -ev
 
 # CHANGE THESE TO YOUR DESIRED ACCOUNT NAME AND DOTFILES_URL
@@ -14,18 +14,49 @@ export DEBIAN_FRONTEND="noninteractive"
 # Install basic packages
 apt-get -y update
 apt-get -y upgrade
-apt-get -y install nginx make fzf sqlite3
+apt-get -y install nginx make fzf sqlite3 php php-fpm
 
 # Install certbot
 snap install --classic certbot
 ln -s /snap/bin/certbot /usr/bin/certbot
 
-# # This is from when I had a custom nginx setuo for node
-# # Leaving it here for whenever I setup one again
-# curl "$NODE_CONF_URL" > /etc/nginx/sites-available/node
-# ln -s /etc/nginx/sites-available/node node
-# rm /etc/nginx/sites-enabled/default
-# systemctl restart nginx
+# Create the nginx config
+cat > /etc/nginx/sites-available/main <<"EOF"
+server {
+    listen 80;
+    server_name your_domain www.your_domain;
+    root /var/www/your_domain;
+
+    index index.html index.htm index.php;
+
+    location / {
+        try_files $uri $uri/ =404;
+    }
+
+    location ~ \.php$ {
+        include snippets/fastcgi-php.conf;
+        fastcgi_pass unix:/var/run/php/php8.1-fpm.sock;
+     }
+
+    location ~ /\.ht {
+        deny all;
+    }
+}
+EOF
+
+# Link the config
+ln -s /etc/nginx/sites-available/main /etc/nginx/sites-enabled/
+unlink /etc/nginx/sites-enabled/default
+
+nginx -t
+systemctl restart nginx
+
+
+cat > /var/www/main/index.php <<"EOF"
+<title>Test Site</title>
+<h1>Verification</h1>
+<?php echo "If you don't see the PHP tags, you're good to go!" ?>
+EOF
 
 # Verify that nginx is serving on port 80
 systemctl status nginx --no-pager --full
@@ -43,20 +74,14 @@ ed /etc/ssh/sshd_config << EOF
 wq
 EOF
 
-# # Install node 18.x
-# curl -fsSL https://deb.nodesource.com/setup_18.x | sudo -E bash -
-# apt-get install -y nodejs
-# npm install -g pm2
-
 # Create non-root user with the authorized ssh keys; note that this is a passwordless account
-useradd -m -s /bin/bash $PERSONAL_USER
+useradd --create-home --shell /bin/bash $PERSONAL_USER
 usermod -aG sudo $PERSONAL_USER
 rsync --archive --chown=$PERSONAL_USER:$PERSONAL_USER ~/.ssh /home/$PERSONAL_USER
 
 # Login to $PERSONAL_USER and install dotfiles
 sudo -i -u $PERSONAL_USER bash << EOF
 git clone $DOTFILES_URL
-cd dotfiles && make
 EOF
 
 echo "You're done!"
