db12154178618554bc3ea8827edf079bfd83dd8a
Alexander Petros <contact@alexpetros.com>
2024-06-07 10:52:14 -0400

    Add migrations to sqlite driver

--- js/sqlite-driver.js
+++ js/sqlite-driver.js
@@ -1,6 +1,9 @@
 import fs from 'node:fs'
 import Database from 'better-sqlite3'
 
+// Match all files with names like 3-migration-name.sql
+const MIGRATION_REGEX = /[0-9]+-.*\.sql/
+
 export default class DatabaseConnection {
   #db
 
@@ -9,9 +12,18 @@ export default class DatabaseConnection {
     try {
       this.#db = new Database(dbName, { fileMustExist: true })
     } catch (err) {
-      console.error(err)
       if (err.code === 'SQLITE_CANTOPEN') {
-        throw new Error(`Failed to open db ${dbName}. Did you remember to initialize the database?`)
+        console.log(`Note: database ${dbName} does not exist; starting new one`)
+        this.#db = new Database(dbName)
+        // Add the migrations table to the new database
+        this.run(`
+          CREATE TABLE _migrations (
+            filename TEXT NOT NULL,
+            timestamp INTEGER DEFAULT CURRENT_TIMESTAMP
+          );
+        `)
+      } else {
+        throw err
       }
     }
 
@@ -72,5 +84,40 @@ export default class DatabaseConnection {
         statement.run(parameters)
       }
     })
+  }
+
+  runMigrations(dirName) {
+    const migrations = fs.readdirSync(dirName, { withFileTypes: true })
+      .filter(item => !item.isDirectory() && MIGRATION_REGEX.test(item.name))
+      .map(item => {
+        // Get the leading number of the migration so that we know what order to run them in
+        const num = parseInt(item.name.match('[0-9]+')?.at(0))
+        const filepath = `${item.path}/${item.name}`
+        // Check whether the migration has been applied before
+        const isApplied = this.get(
+          `SELECT EXISTS (SELECT filename FROM _migrations WHERE filename = ?) as is_applied`,
+          item.name
+        ).is_applied === 1
+
+        return { num, filepath, isApplied, ...item }
+      })
+      .sort((a, b) => (a.num - b.num))
+
+    const unappliedMigrations = migrations.filter(migration => !migration.isApplied)
+    const appliedMigrationsCount = migrations.length - unappliedMigrations.length
+
+    console.log(`Migrations that were previously applied: ${appliedMigrationsCount}`)
+    console.log(`Migrations to apply: ${unappliedMigrations.length}`)
+
+    // Run all the unapplied migrations together in a single transaction
+    // If any of them fail, the database should stay in the same state
+    this.transaction(() => {
+      for (const migration of unappliedMigrations) {
+        console.log(`Applying migration ${migration.name}`)
+        this.execFile(migration.filepath)
+        this.run('INSERT INTO _migrations (filename) VALUES (?)', migration.name)
+      }
+    })()
+
   }
 }
