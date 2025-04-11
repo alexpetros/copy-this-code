3e4c6a595a8c874fac2583416b4b3be13be2a073
Alexander Petros <contact@alexpetros.com>
2025-04-10 21:18:31 -0400

    Click to copy comments

--- js/copy-button.js
+++ js/copy-button.js
@@ -1,4 +1,15 @@
-// Remember to include this script with the "defer" attribute
+/*
+ * A basic "click to copy" button
+ *
+ * Usage: <copy-button>Copy Link</copy-button>
+ * Attributes:
+ *  content - the text that will get copied to the clipboard (defaults to the button's innerText)
+ *  confirmtext - the text that will get displayed on confirmation (defaults to "Copied!")
+ *
+ * When including this script, make sure to use the "defer" or "type=module" attributes.
+ *
+ * Author: Alexander Petros
+ */
 
 class CopyButton extends HTMLElement {
   static DEFAULT_CONFIRMATION_TEXT = 'Copied!'
@@ -9,7 +20,7 @@ class CopyButton extends HTMLElement {
 
     this.innerHTML = ''
 
-    this.content = this.getAttribute('content')
+    this.content = this.getAttribute('content') || this.displayText
     this.confirmText = this.getAttribute('confirmtext') || CopyButton.DEFAULT_CONFIRMATION_TEXT
 
     const button = document.createElement('button')
